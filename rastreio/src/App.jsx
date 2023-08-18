39111f6707f7d1bdade5b1df5a625a8771c32bfb
JovemDevv <anacarolinesan18@gmail.com>
2023-08-18 13:43:34 -0300

    terminando de estilizar

--- rastreio/src/App.jsx
+++ rastreio/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import clienteData from '../dados.json';
 import logo from './assets/logo.png';
 import lupa from './assets/lupa.png';
@@ -20,20 +20,22 @@ function App() {
     <div className="my-container">
       <form>
         <div className="form-group container text-center">
-          <header className="mt-4">
-            <img src={logo} alt="Logo" id="logo" />
+          <header>
+            <img src={logo} alt="Logo" id="logo" className="img-fluid mobile-logo" />
           </header>
           <main className="mt-4">
-            <div className="d-flex flex-column align-items-center">
+            <div className="d-flex flex-column mt-5">
               <p>Consulte sua encomenda:</p>
               <div className="d-flex justify-content-center align-items-center">
                 <div className="input-with-icon">
                   <input
                     type="text"
-                    className="form-control mr-2"
+                    className=" text-center "
                     placeholder="Digite o número do pedido"
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
+                    maxLength={10}
+                   
                   />
                   <img
                     src={lupa}
@@ -47,19 +49,20 @@ function App() {
 
             <div>
               {result !== null && (
-                <div className="mt-4">
+                <div className="mt-5">
                   {result !== false ? (
                     <div>
                       <div className="info-container d-sm-flex flex-column flex-md-row">
                         <div className="mb-3 mb-sm-0 mr-sm-3">
-                          {result.cliente.nome}
+                        {result.id} - {result.cliente.nome}
+                        Número de ordem e nome do cliente 
                         </div>
                         <div className=" mb-sm-0">
                           {new Date(result.data).toLocaleDateString('pt-BR')}
                         </div>
                       </div>
                       <div className="info-container d-sm-flex flex-column flex-md-row">
-                        <div className="mb-3 mb-sm-0 mr-sm-3">
+                        <div className="mb-3 mb-sm-0 mr-sm-3 esconder">
                           R$ {result.valor.toFixed(2)}
                         </div>
                         <div className=" mb-sm-0">
@@ -73,7 +76,9 @@ function App() {
                         <p>Encomenda</p>
                         <p>não encontrada!</p>
                       </div>
+                      <div className="primeiro-p">
                       <p>Procure novamente</p>
+                      </div>
                     </div>
                   )}
                 </div>
