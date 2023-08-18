02222f72a84772e6a807985d90f409b3d13cf305
JovemDevv <anacarolinesan18@gmail.com>
2023-08-18 17:28:41 -0300

    correções no estilo

--- rastreio/src/App.jsx
+++ rastreio/src/App.jsx
@@ -1,19 +1,19 @@
-import  { useState } from 'react';
-import clienteData from '../dados.json';
-import logo from './assets/logo.png';
-import lupa from './assets/lupa.png';
-import './app.scss';
+import { useState } from 'react';
+import clienteData from '../dados.json'; 
+import logo from './assets/logo.png'; 
+import lupa from './assets/lupa.png'; 
+import './app.scss'; 
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [result, setResult] = useState(null);
+  const [search, setSearch] = useState(''); // Estado para o valor de pesquisa
+  const [result, setResult] = useState(null); // Estado para o resultado da pesquisa
 
   const handleSearch = () => {
     const foundOrder = clienteData.encomendas.find(
       (encomenda) => encomenda.numero === search
     );
 
-    setResult(foundOrder || false);
+    setResult(foundOrder || false); // Define o resultado da pesquisa com base no número encontrado
   };
 
   return (
@@ -23,6 +23,7 @@ function App() {
           <header>
             <img src={logo} alt="Logo" id="logo" className="img-fluid mobile-logo" />
           </header>
+          
           <main className="mt-4">
             <div className="d-flex flex-column mt-5">
               <p>Consulte sua encomenda:</p>
@@ -35,7 +36,6 @@ function App() {
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                     maxLength={10}
-                   
                   />
                   <img
                     src={lupa}
@@ -52,21 +52,21 @@ function App() {
                 <div className="mt-5 ">
                   {result !== false ? (
                     <div>
+                      {/* Exibe informações da encomenda encontrada */}
                       <div className="info-container d-sm-flex flex-column flex-md-row ">
                         <div className="mb-3 mb-sm-0 mr-sm-3 ">
-                        {result.id} - {result.cliente.nome}
-                        <span className="line-break">Número de ordem e nome do cliente</span>
+                          {result.id} - {result.cliente.nome}
+                          <span className="line-break">Número de ordem e nome do cliente</span>
                         </div>
                         <div className="mb-3 mb-sm-0 mr-sm-3 esconder ">
                           R$ {result.valor.toFixed(2)}
                           <span className="line-break">Valor do pedido</span>
                         </div>
-                        
                       </div>
                       <div className="inf-container d-sm-flex flex-column flex-md-row">
                         <div className=" mb-sm-0 order ">
                           {new Date(result.data).toLocaleDateString('pt-BR')}
-                        <span className="line-break">Data do pedido</span>
+                          <span className="line-break">Data do pedido</span>
                         </div>
                         <div className=" mb-sm-0 order2" >
                           {result.entregue ? 'Entregue' : 'A entregar'}
@@ -76,12 +76,13 @@ function App() {
                     </div>
                   ) : (
                     <div>
+                      {/* Mensagem exibida quando nenhuma encomenda é encontrada */}
                       <div className="primeiro-p">
                         <p>Encomenda</p>
                         <p>não encontrada!</p>
                       </div>
                       <div className="primeiro-p">
-                      <p>Procure novamente</p>
+                        <p>Procure novamente</p>
                       </div>
                     </div>
                   )}
